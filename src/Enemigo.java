cfbef33195cd3a914334fdb74138dc7be5d935c3
Juan Alcantara <j.alejandrominaya@gmail.com>
2018-03-08 13:47:53 -0600

    Add class Enemigo and constructors

---
+++ src/Enemigo.java
@@ -0,0 +1,28 @@
+public class Enemigo extends Personaje
+{
+    protected boolean esBoss;
+
+    public Enemigo(int vida, Item equipado, int dano, double velocidad, boolean esBoss)
+    {
+        super(vida, equipado, dano, velocidad);
+        this.esBoss = esBoss;
+    }
+
+    public Enemigo(int vida, Item equipado, int dano, double velocidad)
+    {
+        super(vida, equipado, dano, velocidad);
+        esBoss = false;
+    }
+
+    public Enemigo(boolean esBoss)
+    {
+        super();
+        this.esBoss = true;
+        this.vida *= 2;
+    }
+    public Enemigo()
+    {
+        super();
+        esBoss = false;
+    }
+}
